c34e2568bcc11aa7f6b9f0faa4ddf7a967590363
bas-041 <basjen122547@gmail.com>
2026-01-06 10:13:02 +0700

    Revert "Update Home.php with HTML corrections"

--- Home.php
+++ Home.php
@@ -1,4 +1,4 @@
-d<body bgcolor="#CC9900">
+<body bgcolor="#CC9900">
 	<table width="1238" height="530" border="0">
 		<tr>
 			<td width="23" rowspan="2">&nbsp;</td>
@@ -21,7 +21,7 @@ d<body bgcolor="#CC9900">
 					เชิญชมห้องตัวอย่าง ได้ทุกวัน <a href=amit.php>ติดต่อสอบถาม</a>	
 			</td>
 			<td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรกddddddddddddddddddddddddddddddddddddd</li>
+				<li><a href="home.php">หน้าแรก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
@@ -30,5 +30,4 @@ d<body bgcolor="#CC9900">
 			</td>
 		</tr>
 	</table>
-
 <body>
